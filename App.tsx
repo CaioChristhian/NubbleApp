4619f0d5d8ec49e9f63d0d4307df6108791af647
Caio Christhian Lopes Silva <caio.christhian@uft.edu.br>
2024-06-05 13:36:55 -0300

    Fonts Customizadas no Android

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ import {Text} from './src/components/Text/Text';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">Alou</Text>
+      <Text preset="headingLarge" style={{fontFamily: 'Satoshi-MediumItalic'}}>
+        Alo
+      </Text>
     </SafeAreaView>
   );
 }
