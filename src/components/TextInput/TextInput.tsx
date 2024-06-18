a067fd25af562ceaec85f9f3b61a233a3421fa4b
CaioChristhian <caiochristhianz@gmail.com>
2024-06-18 11:03:08 -0300

    feat: style

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,30 +2,44 @@ import React from 'react';
 import {
   TextInput as RNTextInput,
   TextInputProps as RNTextInputProps,
+  TextStyle,
 } from 'react-native';
 import {Box, BoxProps} from '../Box/Box';
-import {Text} from '../Text/Text';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface TextInputProps extends RNTextInputProps {
   label: string;
 }
 
 export function TextInput({label, ...rnTextInputProps}: TextInputProps) {
+  const {colors} = useAppTheme();
   return (
     <Box>
       <Text preset="paragraphMedium" mb="s4">
         {label}
       </Text>
       <Box {...$TextInputContainer}>
-        <RNTextInput {...rnTextInputProps} />
+        <RNTextInput
+          placeholderTextColor={colors.gray2}
+          style={$textInputStyle}
+          {...rnTextInputProps}
+        />
       </Box>
     </Box>
   );
 }
 
+const $textInputStyle: TextStyle = {
+  borderWidth: 1,
+  padding: 0,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
+
 const $TextInputContainer: BoxProps = {
   borderWidth: 1,
-  height: 50,
+  padding: 's16',
   borderColor: 'gray4',
   borderRadius: 's12',
 };
