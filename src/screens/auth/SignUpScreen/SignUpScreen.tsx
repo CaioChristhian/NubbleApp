64deb7c66951c571bd9c46344981d45ace61fe8f
CaioChristhian <caiochristhianz@gmail.com>
2024-06-19 17:39:43 -0300

    SignUp Screen e Botão de Voltar

---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+
+export function SignUpScreen() {
+  function submitForm() {
+    //todo
+  }
+
+  return (
+    <Screen canGoBack>
+      <Text preset="headingLarge" mb="s32">
+        Criar uma conta
+      </Text>
+
+      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
+      <TextInput
+        label="Nome Completo"
+        placeholder="Digite seu nome completo"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
+
+      <TextInput
+        RightComponent={<Icon name="eyeOn" color="gray2" />}
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's48'}}
+      />
+
+      <Button title="Criar uma conta" onPress={submitForm} />
+    </Screen>
+  );
+}
