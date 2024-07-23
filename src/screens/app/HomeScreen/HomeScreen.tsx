ae64df7f61235cfda276ab173a47443b310955f6
CaioChristhian <caiochristhianz@gmail.com>
2024-07-23 11:07:42 -0300

    Lista com FlatList e renderItem

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
 import {Post, postService} from '@domain';
 
-import {Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
   const [postList, setPostList] = useState<Post[]>([]);
 
@@ -12,11 +14,32 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
     postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box mb="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32}}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{width: Dimensions.get('screen').width, height: 300}}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
