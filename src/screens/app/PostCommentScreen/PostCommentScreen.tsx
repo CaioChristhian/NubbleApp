3edfff44f290fefafa547c0174b1653884b348ef
Caio Christhian Lopes Silva <caio.christhian@uft.edu.br>
2024-08-22 13:29:00 -0300

    Type Checker com TypeScript e Husky

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import {usePostCommentList} from '@domain';
+
 import {Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
-export function PostCommentScreen({}: AppScreenProps<'PostCommentScreen'>) {
+export function PostCommentScreen({
+  route,
+}: AppScreenProps<'PostCommentScreen'>) {
+  const postId = route.params.postId;
+  usePostCommentList(postId);
+
   return (
     <Screen title="Comentários" canGoBack>
       <Text preset="headingSmall">Comentarios screen</Text>
