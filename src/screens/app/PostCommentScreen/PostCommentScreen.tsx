d8656dffb9e56b897783397f41d831df03f1ca5a
Caio Christhian Lopes Silva <caio.christhian@uft.edu.br>
2024-08-27 13:41:00 -0300

    Paginação de Comentários

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -4,15 +4,19 @@ import {FlatList, ListRenderItemInfo} from 'react-native';
 import {PostComment, usePostCommentList} from '@domain';
 
 import {Screen} from '@components';
+import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 
+import {PostCommentBottom} from './components';
 import {PostCommentItem} from './components/PostCommentItem';
 
 export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
   const postId = route.params.postId;
-  const {list} = usePostCommentList(postId);
+  const {list, fetchNextPage, hasNextPage} = usePostCommentList(postId);
+
+  const {bottom} = useAppSafeArea();
 
   function renderItem({item}: ListRenderItemInfo<PostComment>) {
     return <PostCommentItem postComment={item} />;
@@ -20,7 +24,18 @@ export function PostCommentScreen({
 
   return (
     <Screen title="Comentários" canGoBack>
-      <FlatList data={list} renderItem={renderItem} />
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={list}
+        renderItem={renderItem}
+        contentContainerStyle={{paddingBottom: bottom}}
+        ListFooterComponent={
+          <PostCommentBottom
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+          />
+        }
+      />
     </Screen>
   );
 }
