cc48901e9d2ec2b1542805b949fd50e92ce325e8
CaioChristhian <caiochristhianz@gmail.com>
2024-08-29 21:49:52 -0300

    Adicionar comentário - Integração com Backend (pt 1) inicio

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.GiQNBOF8yfs3kOB6UUe6X8RuT2ABDBfHwM88bw5PWXnqIEAHJsdvEQJI9WlF',
+      'Bearer Mg.GxLHkZngfTro3fXaxbWTjPqGS7Wo1xlG0VrpAnWeBvGyTQ871OrC3TbupRQb',
   },
 });
