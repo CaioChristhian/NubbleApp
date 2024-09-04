7d4be9d33659dceb65cea419a9d299c38d50e9dc
CaioChristhian <caiochristhianz@gmail.com>
2024-09-04 10:47:51 -0300

    Remover Comentário - useMutation, Hook Customizado para POST e DELETE

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,5 @@
 export * from './Post/postService';
 export * from './Post/postTypes';
 export * from './Post/useCases/usePostList';
-export * from './hooks/usePaginatedList';
 
 export * from './PostComment';
