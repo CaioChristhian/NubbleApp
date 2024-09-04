3ce750964a2412f0f9579cdf760a728f46da6b37
CaioChristhian <caiochristhianz@gmail.com>
2024-09-04 09:13:55 -0300

    Remover Comentário - API e Serviço

--- src/domain/PostComment/postCommentAPI.ts
+++ src/domain/PostComment/postCommentAPI.ts
@@ -2,6 +2,8 @@ import {api, PageAPI, PageParams} from '@api';
 
 import {PostCommentAPI} from './postCommentTypes';
 
+const PATH = 'user/post_comment';
+
 async function getList(
   post_id: number,
   pageParams?: PageParams,
@@ -20,14 +22,23 @@ async function create(
   post_id: number,
   message: string,
 ): Promise<PostCommentAPI> {
-  const response = await api.post<PostCommentAPI>('user/post_comment', {
+  const response = await api.post<PostCommentAPI>(PATH, {
     post_id,
     message,
   });
   return response.data;
 }
 
+async function remove(postCommentId: number): Promise<{message: string}> {
+  const response = await api.delete<{message: string}>(
+    `${PATH}/${postCommentId}`,
+  );
+
+  return response.data;
+}
+
 export const postCommentApi = {
   getList,
   create,
+  remove,
 };
